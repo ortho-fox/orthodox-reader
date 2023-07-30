a80ccca5b708e776a18da399b90188aaecd42132
Fran Swan <fran@boringprotocol.io>
2023-07-30 04:40:04 -0700

    testing books nav

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,29 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Orthodox NKJV Reader</span>,
+  logo: <span>☦️ Orthodox NKJV Reader</span>,
   project: {
     link: 'https://github.com/ortho-fox/orthodox-nkjv-reader',
   },
   docsRepositoryBase: 'https://github.com/ortho-fox/orthodox-nkjv-reader',
   footer: {
-    text: 'Orthodox NKJV Reader',
+    text: '☦️ Orthodox NKJV Reader',
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1, // Specify the folder level at which the menu on the left is collapsed by default
+    autoCollapse: true, // Automatically collapse inactive folders above defaultMenuCollapseLevel
+    toggleButton: true, // Show sidebar toggle button
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return (
+          <div style={{ background: 'cyan', textAlign: 'center' }}>{title}</div>
+        )
+      }
+      if (title === 'Genesis') {
+        return <>📖 {title}</>
+      }
+      return <>{title}</>
+    }
   },
 }
 
