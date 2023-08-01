d3833b5cddf46e5091e356903d04e9bc2f24f727
Fran Swan <fran@boringprotocol.io>
2023-07-31 23:15:52 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 // next.config.js
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  // theme: './theme.tsx', // Path to your custom theme file
   themeConfig: './theme.config.tsx',
 })
 
